302678d7be070e0cc64bbb769505ecd088991053
Mihael Šinkec <sinkec.mihael@gmail.com>
2023-01-11 14:43:01 +0100

    Init empty git repo when testing templates instead of skipping prepare.

--- test-templates.sh
+++ test-templates.sh
@@ -5,7 +5,9 @@ cd templates/
 
 for d in */ ; do
     cd $d
-    npm i --ignore-scripts
+    git init  # Init git repo because of husky so "npm i" passes
+    npm i
+    rm -rf .git/
     npm run lint-check || exit 1
     npm run test || exit 1
     cd ../

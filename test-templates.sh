c9c872db61e46ed450c2b3a8007edc071bc8d9f3
hh <hh@scrypt.io>
2023-03-04 14:36:48 +0800

    test all command #12

--- test-templates.sh
+++ test-templates.sh
@@ -38,16 +38,12 @@ rm -rf hello-world
 npx scrypt-cli project hello-world
 cd hello-world
 
-if tree . | grep -q 'helloWorld.ts'; then
-   echo "find helloWorld.ts"
-else 
+if ! tree . | grep -q 'helloWorld.ts'; then
    echo "cannot find helloWorld.ts"
    exit -1
 fi
 
-if tree . | grep -q 'helloWorld.test.ts'; then
-   echo "find helloWorld.test.ts"
-else 
+if ! tree . | grep -q 'helloWorld.test.ts'; then
    echo "cannot find helloWorld.test.ts"
    exit -1
 fi
@@ -55,4 +51,47 @@ fi
 npm i
 npm t
 npm run genprivkey
+cd ..
 
+
+echo "testing npx scrypt-cli project --state stateful-counter"
+rm -rf stateful-counter
+npx scrypt-cli project --state stateful-counter
+cd stateful-counter
+
+if ! tree . | grep -q 'statefulCounter.ts'; then
+   echo "cannot find statefulCounter.ts"
+   exit -1
+fi
+
+if ! tree . | grep -q 'statefulCounter.test.ts'; then
+   echo "cannot find statefulCounter.test.ts"
+   exit -1
+fi
+npm i
+npm t
+npm run genprivkey
+cd ..
+
+
+echo "testing npx scrypt-cli project --lib my-lib"
+rm -rf my-lib
+npx scrypt-cli project --lib my-lib
+cd my-lib
+
+if ! tree . | grep -q 'myLib.ts'; then
+   echo "cannot find myLib.ts"
+   exit -1
+fi
+
+if ! tree . | grep -q 'myLib.test.ts'; then
+   echo "cannot find myLib.test.ts"
+   exit -1
+fi
+npm i
+npm t
+npm run genprivkey
+cd ..
+
+echo "testing npx scrypt-cli system"
+npx scrypt-cli system

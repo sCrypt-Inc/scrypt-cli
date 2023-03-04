b39aa6891a96fa5f5de8274a3f6f9b55185c0003
hh <hh@scrypt.io>
2023-03-04 16:35:49 +0800

    run more test

--- test-templates.sh
+++ test-templates.sh
@@ -3,36 +3,34 @@ set -e
 
 cd templates/
 
-# for d in */ ; do
-#     cd $d
-#     rm -rf node_modules/ package-lock.json
-#     git init  # Init git repo because of husky so "npm i" passes
-#     npm i
-#     npm run lint
-#     npm run lint-check || exit 1
-#     npm run genprivkey
-#     npx scrypt-cli compile 
-#     npm run test || exit 1
-#     rm -rf .git/
-#     cd ../
-# done
+for d in */ ; do
+    cd $d
+    rm -rf node_modules/ package-lock.json
+    git init  # Init git repo because of husky so "npm i" passes
+    npm i
+    npm run lint
+    npm run lint-check || exit 1
+    npm run genprivkey
+    npx scrypt-cli compile 
+    npm run test || exit 1
+    rm -rf .git/
+    cd ../
+done
 
 
-# echo "testing npx scrypt-cli init ..."
-# rm -rf dapp
-# npx create-react-app dapp --template typescript
-# cd dapp
-# git init
-# git config user.email "ci@scrypt.io"
-# git config user.name "scrypt"
-# git add .
-# git commit -am "Initial commit"
-# npm i
-# npx scrypt-cli init
-# npx scrypt-cli compile 
-# cd ..
-
-npx -v
+echo "testing npx scrypt-cli init ..."
+rm -rf dapp
+npx create-react-app dapp --template typescript
+cd dapp
+git init
+git config user.email "ci@scrypt.io"
+git config user.name "scrypt"
+git add .
+git commit -am "Initial commit"
+npm i
+npx scrypt-cli init
+npx scrypt-cli compile 
+cd ..
 
 if ! node -v | grep -q 'v16.'; then
     npx scrypt-cli -v

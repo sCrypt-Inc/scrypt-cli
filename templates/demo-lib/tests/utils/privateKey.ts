e49914b169d7f314c07271b88d9c14ef9e551751
Mihael Šinkec <sinkec.mihael@gmail.com>
2023-03-06 09:11:19 +0100

    Show address if private key already generated.

--- templates/demo-lib/tests/utils/privateKey.ts
+++ templates/demo-lib/tests/utils/privateKey.ts
@@ -9,16 +9,23 @@ dotenv.config({ path: dotenvConfigPath })
 let privKey = process.env.PRIVATE_KEY
 if (!privKey) {
     genPrivKey()
+} else {
+    showAddr(bsv.PrivateKey.fromWIF(privKey))
 }
 
 export function genPrivKey() {
     const newPrivKey = bsv.PrivateKey.fromRandom('testnet')
     console.log(`Missing private key, generating a new one ...
 Private key generated: '${newPrivKey.toWIF()}'
-You can fund its address '${newPrivKey.toAddress()}' from sCrypt faucet https://scrypt.io/#faucet`)
+You can fund its address '${newPrivKey.toAddress()}' from the sCrypt faucet https://scrypt.io/#faucet`)
     // auto generate .env file with new generated key
     fs.writeFileSync(dotenvConfigPath, `PRIVATE_KEY="${newPrivKey}"`)
     privKey = newPrivKey.toWIF()
+}
+
+export function showAddr(privKey: bsv.PrivateKey) {
+    console.log(`Private key already present ...
+You can fund its address '${privKey.toAddress()}' from the sCrypt faucet https://scrypt.io/#faucet`)
 }
 
 export const myPrivateKey = bsv.PrivateKey.fromWIF(privKey)

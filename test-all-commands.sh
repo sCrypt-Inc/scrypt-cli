a3a85d9c3ef6ff8e676df97d06104f9f7729acab
hh <hf.zheng@scrypt.io>
2023-03-06 16:27:09 +0800

    test all cmd

---
+++ test-all-commands.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+set -e
+
+rm -rf test-commands
+mkdir -p test-commands
+
+cd test-commands
+
+echo "testing get version info"
+node ../src/bin/index.js -v
+
+echo "testing get system info"
+node ../src/bin/index.js system
+
+echo "testing create project hello-world"
+rm -rf hello-world
+node ../src/bin/index.js project hello-world
+cd hello-world
+
+npm i
+node ../../src/bin/index.js compile
+npm t
+npm run genprivkey
+cd ..
+
+
+echo "testing create project --state stateful-counter"
+rm -rf stateful-counter
+node ../src/bin/index.js project --state stateful-counter
+cd stateful-counter
+
+npm i
+node ../../src/bin/index.js compile
+npm t
+npm run genprivkey
+cd ..
+
+
+echo "testing create project --lib my-lib"
+rm -rf my-lib
+node ../src/bin/index.js project --lib my-lib
+cd my-lib
+
+npm i
+node ../../src/bin/index.js compile
+npm t
+npm run genprivkey
+cd ..
+
+
+
+echo "testing init ..."
+rm -rf dapp
+npx create-react-app dapp --template typescript
+cd dapp
+git init
+git config user.email "ci@scrypt.io"
+git config user.name "scrypt"
+git add .
+git commit -am "Initial commit"
+npm i
+node ../../src/bin/index.js init
+node ../../src/bin/index.js compile
+cd ..
---
+++ test-npx.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+set -e
+cd ..
+rm -rf test-npx
+mkdir -p test-npx
+
+cd test-npx
+
+echo "testing get version info"
+npx scrypt-cli -v
+
+echo "testing get system info"
+npx scrypt-cli system
+
+echo "testing create project hello-world"
+rm -rf hello-world
+npx scrypt-cli project hello-world
+cd hello-world
+
+npm i
+npx scrypt-cli compile
+npm t
+cd ..
+
+
+echo "testing create project --state stateful-counter"
+rm -rf stateful-counter
+npx scrypt-cli project --state stateful-counter
+cd stateful-counter
+
+npm i
+npx scrypt-cli compile
+npm t
+cd ..
+
+
+echo "testing create project --lib my-lib"
+rm -rf my-lib
+npx scrypt-cli project --lib my-lib
+cd my-lib
+
+npm i
+npx scrypt-cli compile
+cd ..
+
+
+
+echo "testing init ..."
+rm -rf dapp
+npx create-react-app dapp --template typescript
+cd dapp
+git init
+git config user.email "ci@scrypt.io"
+git config user.name "scrypt"
+git add .
+git commit -am "Initial commit"
+npm i
+npx scrypt-cli init
+npx scrypt-cli compile
+cd ..

478d444d10c5f51347ff33082b972e9a6372c791
hh <hf.zheng@scrypt.io>
2023-07-27 23:16:18 +0800

    Fix scripts

--- src/lib/project.js
+++ src/lib/project.js
@@ -227,8 +227,7 @@ async function configurePackageJson(dir, asm) {
   const packageJson = JSON.parse(fs.readFileSync(packageJsonPath))
 
   if (asm) {
-    packageJson.scripts['postcompile'] = 'npm run apply-asm'
-    packageJson.scripts['apply-asm'] = 'node .asm/apply_asm.js'
+    packageJson.scripts['compile'] = 'npx scrypt-cli compile --asm'
   }
 
   writefile(packageJsonPath, packageJson)

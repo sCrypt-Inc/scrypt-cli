645132a8c20a2efb1aaa62f9906abb053a9258e6
hh <hf.zheng@scrypt.io>
2023-08-16 21:16:24 +0800

    run all test

--- tests/test-all-commands.sh
+++ tests/test-all-commands.sh
@@ -9,109 +9,109 @@ mkdir -p test-commands
 
 cd test-commands
 
-# echo "testing get version info"
-# node ../src/bin/index.js -v
+echo "testing get version info"
+node ../src/bin/index.js -v
 
-# echo "testing get system info"
-# node ../src/bin/index.js system
+echo "testing get system info"
+node ../src/bin/index.js system
 
-# echo "testing create project hello-world"
-# rm -rf hello-world
-# node ../src/bin/index.js project hello-world
-# cd hello-world
+echo "testing create project hello-world"
+rm -rf hello-world
+node ../src/bin/index.js project hello-world
+cd hello-world
 
-# npm i
-# npm i -D ../../
-# npm run compile
-# npm t
-# npm run genprivkey
-# cd ..
-
-
-# echo "testing create project --state stateful-counter"
-# rm -rf stateful-counter
-# node ../src/bin/index.js project --state stateful-counter
-# cd stateful-counter
-
-# npm i
-# npm i -D ../../
-# npm run compile
-# npm t
-# npm run genprivkey
-# cd ..
+npm i
+npm i -D ../../
+npm run compile
+npm t
+npm run genprivkey
+cd ..
 
 
-# echo "testing create project --lib my-lib"
-# rm -rf my-lib
-# node ../src/bin/index.js project --lib my-lib
-# cd my-lib
+echo "testing create project --state stateful-counter"
+rm -rf stateful-counter
+node ../src/bin/index.js project --state stateful-counter
+cd stateful-counter
 
-# npm i
-# npm i -D ../../
-# npm run compile
-# npm t
-# npm run genprivkey
-# cd ..
+npm i
+npm i -D ../../
+npm run compile
+npm t
+npm run genprivkey
+cd ..
 
-# echo "testing create project --asm hello-world-asm"
-# rm -rf hello-world-asm
-# node ../src/bin/index.js project --asm hello-world-asm
-# cd hello-world-asm
 
-# echo '{"HelloWorldAsm": {"unlock": "op_1 op_1 op_equalverify"}}' > .asm/asm.json
-# npm i
-# npm i -D ../../
-# npm run compile
-# npm t
-# npm run genprivkey
-# cd ..
+echo "testing create project --lib my-lib"
+rm -rf my-lib
+node ../src/bin/index.js project --lib my-lib
+cd my-lib
+
+npm i
+npm i -D ../../
+npm run compile
+npm t
+npm run genprivkey
+cd ..
+
+echo "testing create project --asm hello-world-asm"
+rm -rf hello-world-asm
+node ../src/bin/index.js project --asm hello-world-asm
+cd hello-world-asm
+
+echo '{"HelloWorldAsm": {"unlock": "op_1 op_1 op_equalverify"}}' > .asm/asm.json
+npm i
+npm i -D ../../
+npm run compile
+npm t
+npm run genprivkey
+cd ..
 
 chmod +x ../tests/helper-scripts/*.exp
 npm i -g @vue/cli
 
-# echo "testing init React ..."
-# rm -rf dapp-react
-# npx create-react-app@latest dapp-react --template typescript
-# cd dapp-react
-# git init
-# git add .
-# git commit -am "Initial commit"
-# npm i
-# npm i -D ../../
-# npx scrypt-cli init --force
-# npx scrypt-cli compile
-# cp ../../tests/replaced-files/react-index.tsx src/index.tsx
-# cat src/index.tsx
-# CI=true npm t
-# cd ..
+echo "testing init React ..."
+rm -rf dapp-react
+npx create-react-app@latest dapp-react --template typescript
+cd dapp-react
+git init
+git add .
+git commit -am "Initial commit"
+npm i
+npm i -D ../../
+npx scrypt-cli init --force
+npx scrypt-cli compile
+cp ../../tests/replaced-files/react-index.tsx src/index.tsx
+cat src/index.tsx
+CI=true npm t
+cd ..
 
-# echo "testing init Next.js ..."
-# rm -rf dapp-next
-# npx create-next-app dapp-next --typescript --use-npm --eslint --tailwind --src-dir --app --import-alias "@/*"
-# cd dapp-next
-# git init
-# git add .
-# git commit -am "Initial commit"
-# npm i
-# npm i -D ../../
-# npx scrypt-cli init --force
-# npx scrypt-cli compile
-# cp ../../tests/replaced-files/next-page.tsx src/app/page.tsx
-# cat src/app/page.tsx
-# npm run build
-# cd ..
+echo "testing init Next.js ..."
+rm -rf dapp-next
+npx create-next-app dapp-next --typescript --use-npm --eslint --tailwind --src-dir --app --import-alias "@/*"
+cd dapp-next
+git init
+git add .
+git commit -am "Initial commit"
+npm i
+npm i -D ../../
+npx scrypt-cli init --force
+npx scrypt-cli compile
+cp ../../tests/replaced-files/next-page.tsx src/app/page.tsx
+cat src/app/page.tsx
+npm run build
+cd ..
 
-# echo "testing init Angular ..."
-# rm -rf dapp-angular
-# npx @angular/cli new dapp-angular
-# git init
-# git add .
-# git commit -am "Initial commit"
-# npm i
-# npm i -D ../../
-# npx scrypt-cli init --force
-# npx scrypt-cli compile
-# cp ../../tests/replaced-files/next-page.tsx src/app/page.tsx
+echo "testing init Angular ..."
+rm -rf dapp-angular
+npx @angular/cli new dapp-angular
+git init
+git add .
+git commit -am "Initial commit"
+npm i
+npm i -D ../../
+npx scrypt-cli init --force
+npx scrypt-cli compile
+cp ../../tests/replaced-files/next-page.tsx src/app/page.tsx
 
 
 
@@ -130,70 +130,70 @@ cp ../../tests/replaced-files/svelte-page.svelte src/routes/+page.svelte
 cat src/routes/+page.svelte
 npm run build
 
-# echo "testing Vue 3.x bundled with Vite"
-# rm -rf dapp-vue3-vite
-# ../tests/helper-scripts/create-vue3-vite-app.exp
-# cd dapp-vue3-vite
-# git init
-# git add .
-# git commit -am "Initial commit"
-# npm i
-# npm i -D ../../
-# npx scrypt-cli init --force
-# npx scrypt-cli compile
-# cp ../../tests/replaced-files/vue3-vite-main.ts src/main.ts
-# cat src/main.ts
-# npm run build
-# cd ..
+echo "testing Vue 3.x bundled with Vite"
+rm -rf dapp-vue3-vite
+../tests/helper-scripts/create-vue3-vite-app.exp
+cd dapp-vue3-vite
+git init
+git add .
+git commit -am "Initial commit"
+npm i
+npm i -D ../../
+npx scrypt-cli init --force
+npx scrypt-cli compile
+cp ../../tests/replaced-files/vue3-vite-main.ts src/main.ts
+cat src/main.ts
+npm run build
+cd ..
 
-# echo "testing Vue 2.x bundled with Vite"
-# rm -rf dapp-vue2-vite
-# ../tests/helper-scripts/create-vue2-vite-app.exp
-# cd dapp-vue2-vite
-# git init
-# git add .
-# git commit -am "Initial commit"
-# npm i
-# npm i -D ../../
-# npx scrypt-cli init --force
-# npx scrypt-cli compile
-# cp ../../tests/replaced-files/vue2-vite-main.ts src/main.ts
-# cat src/main.ts
-# npm run build
-# cd ..
+echo "testing Vue 2.x bundled with Vite"
+rm -rf dapp-vue2-vite
+../tests/helper-scripts/create-vue2-vite-app.exp
+cd dapp-vue2-vite
+git init
+git add .
+git commit -am "Initial commit"
+npm i
+npm i -D ../../
+npx scrypt-cli init --force
+npx scrypt-cli compile
+cp ../../tests/replaced-files/vue2-vite-main.ts src/main.ts
+cat src/main.ts
+npm run build
+cd ..
 
-# echo "testing Vue 3.x bundled with Webpack"
-# rm -rf dapp-vue3-webpack ~/.vuerc
-# echo "{ \"useTaobaoRegistry\": true }" > ~/.vuerc
-# ../tests/helper-scripts/create-vue3-webpack-app.exp
-# cd dapp-vue3-webpack
-# git init
-# git add .
-# git commit -am "Initial commit"
-# npm i
-# npm i -D ../../
-# npx scrypt-cli init --force
-# npx scrypt-cli compile
-# cp ../../tests/replaced-files/vue3-webpack-main.ts src/main.ts
-# cat src/main.ts
-# npm run build
-# cd ..
+echo "testing Vue 3.x bundled with Webpack"
+rm -rf dapp-vue3-webpack ~/.vuerc
+echo "{ \"useTaobaoRegistry\": true }" > ~/.vuerc
+../tests/helper-scripts/create-vue3-webpack-app.exp
+cd dapp-vue3-webpack
+git init
+git add .
+git commit -am "Initial commit"
+npm i
+npm i -D ../../
+npx scrypt-cli init --force
+npx scrypt-cli compile
+cp ../../tests/replaced-files/vue3-webpack-main.ts src/main.ts
+cat src/main.ts
+npm run build
+cd ..
 
-# rm -rf dapp-vue2-webpack ~/.vuerc
-# echo "{ \"useTaobaoRegistry\": true }" > ~/.vuerc
-# ../tests/helper-scripts/create-vue2-webpack-app.exp
-# cd dapp-vue2-webpack
-# git init
-# git add .
-# git commit -am "Initial commit"
-# npm i
-# npm i -D ../../
-# npx scrypt-cli init --force
-# npx scrypt-cli compile
-# cp ../../tests/replaced-files/vue2-webpack-main.ts src/main.ts
-# cat src/main.ts
-# npm run build
-# cd ..
+rm -rf dapp-vue2-webpack ~/.vuerc
+echo "{ \"useTaobaoRegistry\": true }" > ~/.vuerc
+../tests/helper-scripts/create-vue2-webpack-app.exp
+cd dapp-vue2-webpack
+git init
+git add .
+git commit -am "Initial commit"
+npm i
+npm i -D ../../
+npx scrypt-cli init --force
+npx scrypt-cli compile
+cp ../../tests/replaced-files/vue2-webpack-main.ts src/main.ts
+cat src/main.ts
+npm run build
+cd ..
 
 ls -la
 

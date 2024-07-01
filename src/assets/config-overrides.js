76b66e477b8b791905e2e2427f5440c052b40c5f
hh <hf.zheng@scrypt.io>
2024-07-01 13:51:41 +0800

    Fix #121

--- src/assets/config-overrides.js
+++ src/assets/config-overrides.js
@@ -1,22 +1,34 @@
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = function override(config, env) {
 
-    config.resolve.fallback = {
-        fs: false,
-        os: false,
-        path: false,
-        module: false
-    }
-
-    const scopePluginIndex = config.resolve.plugins.findIndex(
-      ({ constructor }) => constructor && constructor.name === 'ModuleScopePlugin'
-    );
-
-    config.resolve.plugins.splice(scopePluginIndex, 1);
-
-    config.plugins.push(new NodePolyfillPlugin({
-        excludeAliases: ['console']
-      }))
-    return config;
+  config.resolve.fallback = {
+    fs: false,
+    os: false,
+    path: false,
+    module: false
   }
+
+  const scopePluginIndex = config.resolve.plugins.findIndex(
+    ({ constructor }) => constructor && constructor.name === 'ModuleScopePlugin'
+  );
+
+  if(scopePluginIndex > 0) {
+    config.resolve.plugins.splice(scopePluginIndex, 1);
+  }
+
+
+  config.plugins.push(new NodePolyfillPlugin({
+    excludeAliases: ['console']
+  }))
+
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    })
+  )
+
+
+  return config;
+}
